0771c07999e43a0690ce203ac9fd8a1fb4cdf576
Gabriela Papp <gpgabika@gmail.com>
2017-07-24 20:45:15 +0100

    js added

---
+++ app.js
@@ -0,0 +1,10 @@
+ $(document).ready(function () {
+ 	    $("#find-article").click(function (e) {
+
+                    e.preventDefault();
+
+                    var option = $("#search-result option:selected").val();
+                    var api_key= 'Place for your own API key.';
+                     var api = "https://api.nytimes.com/svc/topstories/v2/" + option + ".json?api-key="+ api_key;
+                });
+ });
