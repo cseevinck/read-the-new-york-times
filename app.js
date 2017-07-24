547bf53286e4c27ce806b7a6668e274ac2c41e20
Gabriela Papp <gpgabika@gmail.com>
2017-07-24 22:17:56 +0100

    new functionality added

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
- $( document ).ready(function(){
- 	    $("#find-article").click(function (e) {
+      $(document).ready(function () {
+                var api_key = '80deeaf2386f4085831eb911e4ac9837';
+                var api = "https://api.nytimes.com/svc/topstories/v2/world.json?api-key=" + api_key;
+                $.ajax({
+                    url: api,
+                    method: 'GET'
+                }).done(function (data) {
+                    var section = data.results[0].section;
+                    $("#section").html('<p class="right">' + section + '</p>');
+                    $('<hr>').insertBefore("#title");
 
-                    e.preventDefault();
+                    var title = data.results[0].title;
+                    $("#title").html('<p>' + title + '</p>');
+                    var description = data.results[0].abstract;
+                    $("#desc").html('<p class="tm">' + description + '</p>');
+                    var result = data.results[0].multimedia[2].url;
+                    $("#image").html('<div class="img"><img src="' + result + '"></div>');
 
-                    var option = $("#search-result option:selected").val();
-                    var api_key= 'Place your own API key here.';
-                     var api = "https://api.nytimes.com/svc/topstories/v2/" + option + ".json?api-key="+ api_key;
-                            $.ajax({
-                        url: api,
-                        method: 'GET'
-                    }).done(function (data) {
-                        var section = data.results[0].section;
-                        $("#section").html('<p class="tm">' + section + '</p>');
-                        var title = data.results[0].title;
-                        $("#title").html('<p class="tm">' + title + '</p>');
-                        var description = data.results[0].abstract;
-                        $("#desc").html('<p class="tm">' + description + '</p>');
-                        var result = data.results[0].multimedia[2].url;
-                        $("#result").html('<div class="tm"><img src="' + result + '"></div>');
-
+                    $("#find-article").click(function (e) {
+                        e.preventDefault();
+                        option = $("#search-result option:selected").val();
+                        api = "https://api.nytimes.com/svc/topstories/v2/" + option + ".json?api-key=" + api_key;
+                        $.ajax({
+                            url: api,
+                            method: 'GET'
+                        }).done(function (data) {
+                            var section = data.results[0].section;
+                            $("#section").html('<p class="right">' + section + '</p>');
+                            
+                            var title = data.results[0].title;
+                            $("#title").html('<p>' + title + '</p>');
+                            var description = data.results[0].abstract;
+                            $("#desc").html('<p class="tm">' + description + '</p>');
+                            var result = data.results[0].multimedia[2].url;
+                            $("#image").html('<div class="img"><img src="' + result + '"></div>');
+                        });
                     });
                 });
- });
+            });
